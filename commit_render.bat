9b1114fb8588e3c3269dda384f3da2e08ffea4aa
muskan73386 <muskan.73386@gmail.com>
2026-04-05 17:28:48 +0530

    Fix Render blueprint: remove rootDirectory and update commands

--- commit_render.bat
+++
@@ -1,3 +0,0 @@
-git add .
-git commit -m "Add Render Blueprint configuration"
-git push origin main
---
+++ fix_render.bat
@@ -0,0 +1,3 @@
+git add .
+git commit -m "Fix Render blueprint: remove rootDirectory and update commands"
+git push origin main
